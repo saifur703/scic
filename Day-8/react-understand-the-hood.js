62d60d77ae4548590475f4837053b99635e2a310
saifur703 <saifur703@gmail.com>
2020-05-04 22:11:45 +0600

    Milestone-2 Day-1

---
+++ Day-8/react-understand-the-hood.js
@@ -0,0 +1,84 @@
+/**
+ * -- 1 --
+ * What is REACT?
+ * 1. React is a Javascript library; not a framework
+ * 2. It's not a complete solutions. sometimes we need to use more dependency with React.
+ *
+ */
+
+/**
+ * -- 2 --
+ * What is Webpack?
+ * 1. Webpack is a command line tool to create bundles of assets
+ * 2. Webpack doesn’t run on the server or the browser
+ *
+ */
+
+/**
+ * -- 3 --
+ * What is Node?
+ * 1. DescriptionNode.js is an open-source, cross-platform, JavaScript runtime environment
+ * 2. It's executes JavaScript code outside of a web browser
+ *
+ */
+
+/**
+ * -- 4 --
+ * What is NPM?
+ * 1. NPM is a package manager for the JavaScript programming language
+ * 2. It is the default package manager for the JavaScript runtime environment Node.js.
+ *
+ */
+
+/**
+ * -- 5 --
+ * What is JSX?
+ * 1. JSX stands for JavaScript XML
+ * 2. JSX is an XML/HTML-like syntax used by React that extends ECMAScript
+ * 3. JSX makes it easier to write and add HTML in React.
+ */
+
+/**
+ * -- 6 --
+ * What is Babel?
+ * 1. DescriptionBabel is a free and open-source JavaScript trans compiler
+ * 2. used to convert ECMAScript 2015
+ * 3. Babel is a popular tool for using the newest features of the JavaScript programming language
+ *
+ */
+
+/**
+ * -- 7 --
+ * What is Virtual DOM?
+ * 1. Virtual DOM is a node tree
+ * 2. that lists elements and their attributes and content as objects and properties
+ * 3. The Virtual DOM is an abstraction of the HTML DOM
+ */
+
+/**
+ * -- 8 --
+ * What is REACT Components?
+ * 1. Components are the building blocks of any React app
+ *
+ * there are two types of components in React
+ * ** Functional Component and Class based Component
+ *
+ * A functional component is just a plain JavaScript function which accepts props as an argument and returns a React element.
+ *
+ * A class component requires you to extend from React. Component and create a render function which returns a React element
+ */
+
+/**
+ * -- 9 --
+ * What is props?
+ * 1. Props is a special keyword in React
+ * 2. stands for properties and is being used for passing data from one component to another
+ * 3. data with props are being passed in a uni-directional flow. ( from parent to child)
+ */
+
+/**
+ * -- 10 --
+ * What is Hooks?
+ * React Hooks are functions that let us hook into the React state and lifecycle features from function components.
+ *  we mean that hooks allow us to easily manipulate the state of our functional component without needing to convert them into class components
+ */
